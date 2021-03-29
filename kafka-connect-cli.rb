e9b38b46f150ebebdda3b918ff26d1161e58d3c3
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-30 02:54:17 +0900

    Brew formula update for kafka-connect-cli version v0.0.4

--- kafka-connect-cli.rb
+++ kafka-connect-cli.rb
@@ -2,16 +2,16 @@
 class KafkaConnectCli < Formula
   desc ""
   homepage ""
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lala7573/kafka-connect-cli/releases/download/v0.0.3/kafka-connect-cli_0.0.3_Darwin_x86_64.tar.gz"
-    sha256 "d759e1a38dc5498bdd11085ea3ab40b5eb754b42b79459735140b7302e930647"
+    url "https://github.com/lala7573/kafka-connect-cli/releases/download/v0.0.4/kafka-connect-cli_0.0.4_Darwin_x86_64.tar.gz"
+    sha256 "00bef5e3f52cfe5e1f8a635002c90f11366c6d821e9eb49044cf510e99e94e2f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lala7573/kafka-connect-cli/releases/download/v0.0.3/kafka-connect-cli_0.0.3_Linux_x86_64.tar.gz"
-      sha256 "fde8994afa3a84fa1daba2697a8aeb27791c0b9e78f5108b729498e35391bebc"
+      url "https://github.com/lala7573/kafka-connect-cli/releases/download/v0.0.4/kafka-connect-cli_0.0.4_Linux_x86_64.tar.gz"
+      sha256 "ac20e10546ff4ec42fad3ba33e8db5a85aee6a0044577353d92a3e4989b320f5"
     end
   end
 
